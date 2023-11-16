1ea157b9f3db16a2f5b11297fa9db8cae38cf728
B. Yauris <byahux@gmail.com>
2023-11-16 14:54:32 -0500

    mod

--- sockets/socket.js
+++ sockets/socket.js
@@ -2,20 +2,21 @@
 const {io} = require('../index');
 var count = 0;
 io.on('connection', client => {
-    console.log('Cliente conectado');
-    count++;
-    io.emit('web', count);
-    
-    client.on('disconnect', () => { 
-        console.log('Cliente desconectado');
-        count--;
-        io.emit('web', count);
-        
+  console.log('Cliente conectado');
+  count++;
+  io.emit('web', count);
+  
+  client.on('disconnect', () => { 
+      console.log('Cliente desconectado');
+      count--;
+      io.emit('web', count);
+      
   });
   
   client.on('mensaje', ( payload ) => {
     console.log('MENSAJE!!!', payload);
     io.emit('mensaje', payload);
+    // console.log(payload);
     
   });
 
